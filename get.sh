646d713ea79c4db12542879c1c52913dd7bd70fa
tanuj108 <71034587+tanuj108@users.noreply.github.com>
2020-10-05 10:01:10 +0530

    Update get.sh

--- get.sh
+++ get.sh
@@ -2,5 +2,5 @@
 
 echo "hi, we are in get.sh.......this script will get the specified file/directory from	 the s3bucket"
 
-LATEST_CASSANDARA_FILE=`s3cmd ls "$S3_PATH" --recursive | sort | tail -1 | awk '{print $4}'`;
-/usr/local/bin/s3cmd get $LATEST_CASSANDARA_FILE "$DATA_PATH"
+LATEST_FILE=`s3cmd ls "$S3_PATH" --recursive | sort | tail -1 | awk '{print $4}'`;
+/usr/local/bin/s3cmd get $LATEST_FILE "$DATA_PATH"
